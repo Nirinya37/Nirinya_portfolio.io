2a99209ff058a8ea00a39023844d491acb6c9a94
Nirinya37 <164380@wsk.ac.th>
2026-07-18 19:11:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const cards=document.querySelectorAll(".card");
 
+const observer=new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity=1;
+
+entry.target.style.transform="translateY(0px)";
+
+}
+
+})
+
+})
+
+cards.forEach(card=>{
+
+card.style.opacity=0;
+
+card.style.transform="translateY(50px)";
+
+card.style.transition="1s";
+
+observer.observe(card);
+
+})
